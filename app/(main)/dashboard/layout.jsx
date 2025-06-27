4c58f4ba5d569e1e322080bc91bc2491c59c64c6
Abhirup Ghosh <codewithabhi2024@gmail.com>
2025-06-27 18:41:18 +0530

    implement budget progress

--- app/(main)/dashboard/layout.jsx
+++ app/(main)/dashboard/layout.jsx
@@ -1,6 +1,6 @@
 const layout = ({children}) => {
   return (
-    <div className="px-5">
+    <div className="px-5 w-11/12 md:w-3/4 mx-auto">
       <h1 className="text-6xl font-bold gradient gradient-title mb-5">
         Dashboard
       </h1>
--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,18 +1,35 @@
+import { getCurrentBudget } from "@/actions/budget";
 import { getuserAccounts } from "@/actions/dashboard";
 import { AccountCard } from "@/components/account-card";
 import AccountGridModal from "@/components/account-grid-modal";
+import { BudgetProgress } from "@/components/BudgetProgress";
+
 export const dynamic = "force-dynamic";
 
 const DashboardPage = async () => {
+  let budgetData = null;
   const accounts = await getuserAccounts();
+
+  const defaultAccount = accounts?.find((account) => account.isDefault);
+
+  if (defaultAccount) {
+    budgetData = await getCurrentBudget(defaultAccount.id);
+  }
+  console.log("Budget Data", budgetData)
   return (
     <div className="px-5">
       {/* Budget Progress */}
+      {defaultAccount && (
+        <BudgetProgress
+          initialBudget={budgetData?.budget}
+          currentExpenses={budgetData?.currentExpenses || 0}
+        />
+      )}
 
       {/* Overview */}
 
       {/* Accounts Grid */}
-      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3 my-10">
         <AccountGridModal />
         {accounts?.length > 0 &&
           accounts?.map((account) => {
